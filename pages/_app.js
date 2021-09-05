b845c38da9acb5cdb7be87eed3a4eb2e6a93353f
Ladji CISSE <ladji.cis@gmail.com>
2021-09-05 07:44:15 +0200

    Assets, Metadata, and CSS

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "../styles/global.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  )
+}
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,21 @@
 import Link from "next/link";
+import Head from "next/head";
+
+import Layout from "../../components/layout";
+
+import Alert from "../../components/alert";
 
 function FirstPost() {
   return (
-    <div>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </div>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <div>
+        <h1>First Post</h1>
+        <Alert type="success">Hello</Alert>
+      </div>
+    </Layout>
   )
 }
 
